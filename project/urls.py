ce11a2a4923cba921538e4b63b1c4f3a930d51e6
Tim Saylor <tim.saylor@gmail.com>
2010-03-20 16:44:29 -0500

    initial stuff.  working on the mockup.

---
+++ project/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^twitter/', include('twitter_app.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^$|^(.*)/$', 'feincms.views.base.handler'),
+)
+
+from django.conf import settings
+if getattr(settings, 'MEDIA_SERVE', False):
+   urlpatterns = patterns('',
+      (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':
+settings.MEDIA_ROOT}),
+   ) + urlpatterns
+
