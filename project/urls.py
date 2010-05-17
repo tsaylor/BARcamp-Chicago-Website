9809a04d4afe8cc2f0ce20dda5bdd668b1e32b07
Dan <orblivion@OKComputer.(none)>
2010-05-16 20:11:36 -0500

    Adding a URL to make wiki editing work. This was a lot more work than it looks like ;)

--- project/urls.py
+++ project/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+groupdict = {'groupName': 'Barcamp'}
+
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
@@ -12,7 +14,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^wiki/', include('sphene.sphwiki.urls'), {'groupName': 'Barcamp'}),
+    url(r'^wiki/', include('sphene.sphwiki.urls'), groupdict), 
+    url(r'^community/', include('sphene.community.urls'), groupdict),
     url(r'^$|^(.*)/$', 'feincms.views.base.handler'),
 )
 
