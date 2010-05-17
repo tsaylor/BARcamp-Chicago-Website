b21442560383b7336bd66386a16bcb4b6da8ffd2
Tim Saylor <tsaylor@psclistens.com>
2010-05-17 01:06:13 -0500

    Added a dumb entry model to circumvent the permalink issue.  Smart entry work moved to a branch

--- website/entry_urls.py
+++ website/entry_urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls.defaults import *
-from feincms.module.blog.models import Entry
+#from feincms.module.blog.models import Entry
+from website.models import Entry
 
 entry_dict = {'queryset': Entry.objects.all()}
 
 urlpatterns = patterns('',
+   url(r'^news/(?P<object_id>\d+)/',
+       'feincms.views.generic.list_detail.object_detail',
+       dict(entry_dict, **{'template_name': 'news/entry_detail.html'}),
+       name='blog_entry_detail'),
    url(r'^$',
        'feincms.views.generic.list_detail.object_list',
        dict(entry_dict, **{'template_name': 'news/entry_list.html'}),
        name='blog_entry_list'),
-   url(r'^news/(?P<object_id>\d+)/$',
-       'feincms.views.generic.list_detail.object_detail',
-       dict(entry_dict, **{'template_name': 'news/entry_detail.html'}),
-       name='blog_entry_detail'),
 )
 
