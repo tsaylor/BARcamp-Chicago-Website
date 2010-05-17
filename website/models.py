56bd6b034ad91ad46bc5ec25b820c347af673b52
Dan <orblivion@OKComputer.(none)>
2010-05-16 21:41:03 -0500

    Added Wiki content type. Pretty much works, but it flakes out when there's a wiki link to render.

--- website/models.py
+++ website/models.py
@@ -8,6 +8,9 @@ from feincms.content.richtext.models import RichTextContent
 from feincms.content.image.models import ImageContent
 from feincms.content.application.models import ApplicationContent
 
+from feincms.content.application.models import ApplicationContent
+from sphene.sphwiki.models import WikiSnip
+
 
 class TextilePageContent(models.Model):
     content = models.TextField()
@@ -17,6 +20,19 @@ class TextilePageContent(models.Model):
 
     def render(self, **kwargs):
         return textile(self.content)
+
+
+class SpheneWikiPageContent(models.Model):
+    snip = models.ForeignKey(WikiSnip)
+
+    class Meta:
+        abstract = True
+
+    def render(self, **kwargs):
+        if self.snip != None:
+            return self.snip.render()
+
+
 
 # feincms page stuff
 Page.register_extensions('datepublisher', 'translations') # Example set of extensions
@@ -41,6 +57,7 @@ Page.create_content_type(ApplicationContent, APPLICATIONS=(
     ))
 
 Page.create_content_type(TextilePageContent)
+Page.create_content_type(SpheneWikiPageContent)
 
 
 
