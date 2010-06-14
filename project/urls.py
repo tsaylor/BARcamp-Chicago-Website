e1d6cc393223567dca70390884bccd1451cbf79a
Tim Saylor <tsaylor@psclistens.com>
2010-06-14 14:02:45 -0500

    tried to incorporate filebrowser, most of it's commented out.  didn't work

--- project/urls.py
+++ project/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+#    url(r'^admin/filebrowser/', include('filebrowser.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^wiki/', include('sphene.sphwiki.urls'), groupdict), 
--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from website.models import Entry
+from tinymce.widgets import AdminTinyMCE
+from django.db import models
 
-admin.site.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminTinyMCE},
+    }
+
+
+admin.site.register(Entry, EntryAdmin)
